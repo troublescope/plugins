0453c51d2ade56299c85ef8b2fe69cd5e9083321
tomyprs <tomiprs25@gmail.com>
2023-04-15 15:20:41 +0700

    refactor(mentions): move toggle and database operations to MentionsDB class feat(mentions): add MentionsDB class to handle database operations and toggle The MentionsDB class is created to handle database operations and toggle. The toggle_mentions function is refactored to use the MentionsDB class to update the toggle and get the toggle text. The handle_mentions function is refactored to use the MentionsDB class to save mentions. This improves the code structure and makes it easier to maintain.

--- plugins/utils/mentions/__main__.py
+++ plugins/utils/mentions/__main__.py
@@ -1,30 +1,73 @@
-""" Mentions alerter Plugin """
-
-# Copyright (C) 2020-2022 by UsergeTeam@Github, < https://github.com/UsergeTeam >.
-#
-# This file is part of < https://github.com/UsergeTeam/Userge > project,
-# and is released under the "GNU v3.0 License Agreement".
-# Please see < https://github.com/UsergeTeam/Userge/blob/master/LICENSE >
-#
-# All rights reserved.
-
 from pyrogram.errors import PeerIdInvalid
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 from pyrogram import enums
 
-from userge import userge, Message, config, filters, get_collection
+from userge import userge, Message, filters, get_collection
 
-SAVED_SETTINGS = get_collection("CONFIGS")
-TOGGLE = False
+class MentionsDB:
+    def __init__(self):
+        self.db = get_collection("CONFIGS")
+        data = self.db.find_one({"_id": "MENTION_TOGGLE"})
+        self.toggle = bool(data.get("data")) if data else False
+
+    def update_toggle(self):
+        self.toggle = not self.toggle
+        self.db.update_one(
+            {"_id": "MENTION_TOGGLE"}, {"$set": {"data": self.toggle}}, upsert=True
+        )
+
+    async def save_mention(self, msg: Message):
+        if not self.toggle:
+            return
+        if not msg.from_user or msg.from_user.is_verified:
+            return
+        if msg.chat.type == enums.ChatType.PRIVATE:
+            link = f"tg://openmessage?user_id={msg.chat.id}&message_id={msg.id}"
+            text = f"{msg.from_user.mention} 💻 sent you a **Private message.**"
+        else:
+            link = msg.link
+            text = f"{msg.from_user.mention} 💻 tagged you in **{msg.chat.title}.**"
+        text += f"\n\n[Message]({link}):" if not userge.has_bot else "\n\n**Message:**"
+        if msg.text:
+            text += f"\n`{msg.text}`"
+        button = InlineKeyboardButton(text="📃 Message Link", url=link)
+        client = userge.bot if userge.has_bot else userge
+        try:
+            await client.send_message(
+                chat_id=userge.id if userge.has_bot else config.LOG_CHANNEL_ID,
+                text=text,
+                disable_web_page_preview=True,
+                reply_markup=InlineKeyboardMarkup([[button]])
+            )
+            if not msg.text:
+                await client.copy_message(
+                    userge.id if userge.has_bot else config.LOG_CHANNEL_ID,
+                    msg.chat.id,
+                    msg.id
+                )
+        except PeerIdInvalid:
+            if userge.dual_mode:
+                await userge.send_message(userge.id, "/start")
+                await client.send_message(
+                    chat_id=userge.id if userge.has_bot else config.LOG_CHANNEL_ID,
+                    text=text,
+                    disable_web_page_preview=True,
+                    reply_markup=InlineKeyboardMarkup([[button]])
+                )
+                if not msg.text:
+                    await client.copy_message(
+                        userge.id if userge.has_bot else config.LOG_CHANNEL_ID,
+                        msg.chat.id,
+                        msg.id
+                    )
+            else:
+                raise
+
+    async def get_toggle_text(self):
+        return f"Mentions Alerter **{'enabled' if self.toggle else 'disabled'}** Successfully."
 
 
-@userge.on_start
-async def _init():
-    global TOGGLE  # pylint: disable=global-statement
-    data = await SAVED_SETTINGS.find_one({"_id": "MENTION_TOGGLE"})
-    if data:
-        TOGGLE = bool(data["data"])
-
+MENTIONS_DB = MentionsDB()
 
 @userge.on_cmd(
     "mentions",
@@ -33,60 +76,13 @@ async def _init():
 )
 async def toggle_mentions(msg: Message):
     """ toggle mentions """
-    global TOGGLE  # pylint: disable=global-statement
-    if TOGGLE:
-        TOGGLE = False
-    else:
-        TOGGLE = True
-    await SAVED_SETTINGS.update_one(
-        {"_id": "MENTION_TOGGLE"}, {"$set": {"data": TOGGLE}}, upsert=True
-    )
-    await msg.edit(f"Mentions Alerter **{'enabled' if TOGGLE else 'disabled'}** Successfully.")
+    MENTIONS_DB.update_toggle()
+    toggle_text = await MENTIONS_DB.get_toggle_text()
+    await msg.edit(toggle_text)
 
 
 @userge.on_filters(
     ~filters.me & ~filters.bot & ~filters.service
     & (filters.mentioned | filters.private), group=1, allow_via_bot=False)
 async def handle_mentions(msg: Message):
-
-    if TOGGLE is False:
-        return
-
-    if not msg.from_user or msg.from_user.is_verified:
-        return
-
-    if msg.chat.type == enums.ChatType.PRIVATE:
-        link = f"tg://openmessage?user_id={msg.chat.id}&message_id={msg.id}"
-        text = f"{msg.from_user.mention} 💻 sent you a **Private message.**"
-    else:
-        link = msg.link
-        text = f"{msg.from_user.mention} 💻 tagged you in **{msg.chat.title}.**"
-    text += f"\n\n[Message]({link}):" if not userge.has_bot else "\n\n**Message:**"
-    if msg.text:
-        text += f"\n`{msg.text}`"
-
-    button = InlineKeyboardButton(text="📃 Message Link", url=link)
-
-    client = userge.bot if userge.has_bot else userge
-    try:
-        await client.send_message(
-            chat_id=userge.id if userge.has_bot else config.LOG_CHANNEL_ID,
-            text=text,
-            disable_web_page_preview=True,
-            reply_markup=InlineKeyboardMarkup([[button]])
-        )
-        if not msg.text:
-            await msg.copy(userge.id if userge.has_bot else config.LOG_CHANNEL_ID)
-    except PeerIdInvalid:
-        if userge.dual_mode:
-            await userge.send_message(userge.id, "/start")
-            await client.send_message(
-                chat_id=userge.id if userge.has_bot else config.LOG_CHANNEL_ID,
-                text=text,
-                disable_web_page_preview=True,
-                reply_markup=InlineKeyboardMarkup([[button]])
-            )
-            if not msg.text:
-                await msg.copy(userge.id if userge.has_bot else config.LOG_CHANNEL_ID)
-        else:
-            raise
+    await MENTIONS_DB.save_mention(msg)
