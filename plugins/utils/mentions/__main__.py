39993274fcaeaab846d1f07941ae79d994edd837
tomyprs <tomiprs25@gmail.com>
2023-04-15 15:36:33 +0700

    fix(mentions): add await keyword to db find_one and update_one methods

--- plugins/utils/mentions/__main__.py
+++ plugins/utils/mentions/__main__.py
@@ -7,12 +7,12 @@ from userge import config, userge, Message, filters, get_collection
 class MentionsDB:
     def __init__(self):
         self.db = get_collection("CONFIGS")
-        data = self.db.find_one({"_id": "MENTION_TOGGLE"})
+        data = await self.db.find_one({"_id": "MENTION_TOGGLE"})
         self.toggle = bool(data.get("data")) if data else False
 
     def update_toggle(self):
         self.toggle = not self.toggle
-        self.db.update_one(
+        await self.db.update_one(
             {"_id": "MENTION_TOGGLE"}, {"$set": {"data": self.toggle}}, upsert=True
         )
 
